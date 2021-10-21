3a9d0738bca17659dfc2f87317085167f746a2a3
Abolfazl Panbehkar <abolfazl.panbehkar@gmail.com>
2021-10-21 20:33:12 +0330

    feat: :art: Change secondary font

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,7 +32,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
-        fonts: [`Nunito`, `Vollkorn`],
+        fonts: [`Nunito`, `Kanit`],
         display: "swap",
       },
     },
